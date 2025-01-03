c01fa276ca7ca85f2c5368102d90f0ad5f3d7217
Victor Sakamoto <victorysyama@gmail.com>
2025-01-03 12:34:14 -0300

    echo 2

--- echo/cmd/main.go
+++ echo/cmd/main.go
@@ -5,12 +5,12 @@ import (
 	"os"
 )
 
-// echo1
+// echo2
 func main() {
 
-	var s, sep string
-	for i := 0; i < len(os.Args); i++ {
-		s += sep + os.Args[i]
+	s, sep := "", ""
+	for _, arg := range os.Args[0:] {
+		s += sep + arg
 		sep = " "
 	}
 	fmt.Println(s)
